09be1f68725b378bd871b841f5096ecc030af70b
ivanlab <ivanlab@gmail.com>
2013-03-26 23:25:58 +0100

    rsync con lock

--- synchro.sh
+++ synchro.sh
@@ -5,9 +5,17 @@
 #
 #  Created by Ivan Padilla Ojeda on 2013.03.24.
 #
+# Parametros rsync
 NASUSER=ivan
 NASHOST=nas.ivanlab.org
 NASPATH=/media/NAS/00_Raspbian_Sync/
-LPATH=/home/pi/TimeLapse/Fotos
+LPATH=/mnt/tera/Fotos
 
-rsync -az -e "ssh -p 7998" $LPATH $NASUSER@$NASHOST:$NASPATH
+# CUERPO
+
+if [ ! -f /tmp/rsync.lock ]
+then
+    touch /tmp/rsync.lock
+    /usr/bin/rsync -az -e "ssh -p 7998" $LPATH $NASUSER@$NASHOST:$NASPATH
+    rm /tmp/rsync.lock
+fi
