72bde18274f7bd0ac6bb0c5527b9d354a0bf1ddd
Ivan Padilla <ivan@ivanlab.org>
2015-11-26 12:07:04 +0100

    modified synchro to report lock

--- synchro.sh
+++ synchro.sh
@@ -14,15 +14,16 @@ LPATH=/media/tera/Fotos_JPG/
 
 # CUERPO
 if ! ping -c 3 8.8.8.8
- then
- 	ifdown wlan0 && ifup wlan0
- else
-    if [ ! -f /tmp/rsync.lock ]
-		then
-		    touch /tmp/rsync.lock
-		    rsync -avz -e "ssh -p 6666 -i /home/pi/.ssh/id_rsa" LPATH $NASUSER@$NASHOST:$NASPATH
-		    echo ok
-		    rm /tmp/rsync.lock
+	then
+	 	ifdown wlan0 && ifup wlan0
+	else
+	    if [ ! -f /tmp/rsync.lock ]
+			then
+			    touch /tmp/rsync.lock
+			    rsync -avz -e "ssh -p 6666 -i /home/pi/.ssh/id_rsa" LPATH $NASUSER@$NASHOST:$NASPATH
+			    echo ok
+			    rm /tmp/rsync.lock
+			else echo "rsync is locked by /tmp/rsync.lock"
 		fi
  fi
 
